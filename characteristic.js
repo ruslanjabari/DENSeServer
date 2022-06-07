cf108f8bef8369af980c7a16778b9198fc6d2a14
Ruslan <rjabari@stanford.edu>
2022-06-07 01:02:11 -0700

    your bad

--- characteristic.js
+++ characteristic.js
@@ -1,7 +1,7 @@
 var util = require('util');
 var bleno = require('bleno');
 
-function DENSeCharacteristic() {
+function DENSeCharacteristic1() {
   bleno.Characteristic.call(this, {
     uuid: '20000000-0000-0000-0000-000000000001'.replace(/\-/gi, ''),
     properties: ['read', 'write', 'notify', 'writeWithoutResponse'],
@@ -13,14 +13,12 @@ function DENSeCharacteristic() {
     ],
   });
   this._tmpMessages = '';
-  this._firstHalf = '';
-  this_secondHalf = '';
   this._updateValueCallback = null;
 }
 
-util.inherits(DENSeCharacteristic, bleno.Characteristic);
+util.inherits(DENSeCharacteristic1, bleno.Characteristic);
 
-DENSeCharacteristic.prototype.onWriteRequest = function (data, offset, withoutResponse, callback) {
+DENSeCharacteristic1.prototype.onWriteRequest = function (data, offset, withoutResponse, callback) {
   console.log('inbound write req');
 
   if (offset) {
@@ -38,15 +36,83 @@ DENSeCharacteristic.prototype.onWriteRequest = function (data, offset, withoutRe
     this._updateValueCallback(data);
   }
 
-  if (data.toString().includes('header-1')) {
-    this._firstHalf = data.toString();
-    console.log('first half:', this._firstHalf);
-    if (data.toString().includes('header-2')) {
-      this._secondHalf = data.toString();
-      console.log('second half:', this._secondHalf);
-    }
-    callback(this.RESULT_SUCCESS);
+  // store data, then write response
+  if (data.toString().includes('{') && data.toString().includes('header')) {
+    this._tmpMessages = data;
+  } else {
+    this._tmpMessages += data;
+  }
+  console.log('storing messages:', data.toString());
+  callback(this.RESULT_SUCCESS);
+};
+
+DENSeCharacteristic1.prototype.onReadRequest = function (offset, callback) {
+  console.log('incoming read req');
+
+  if (offset) {
+    callback(this.RESULT_ATTR_NOT_LONG, null);
     return;
+  }
+
+  // get data, then write response
+  if (!this._tmpMessages) {
+    console.log('you have no messages stored');
+    callback(this.RESULT_SUCCESS, Buffer.from('EOF', 'utf8'));
+  } else {
+    const data = this._tmpMessages.toString();
+    console.log('getting messages:', data);
+    data.replace('undefined', '');
+    callback(this.RESULT_SUCCESS, Buffer.from(data, 'utf8'));
+  }
+};
+
+// handle on subscribe
+DENSeCharacteristic1.prototype.onSubscribe = function (maxValueSize, updateValueCallback) {
+  console.log('on subscribe');
+
+  this._updateValueCallback = updateValueCallback;
+};
+
+// handle on unsubscribe
+DENSeCharacteristic1.prototype.onUnsubscribe = function () {
+  console.log('on unsubscribe');
+
+  this._updateValueCallback = null;
+};
+
+function DENSeCharacteristic2() {
+  bleno.Characteristic.call(this, {
+    uuid: '20000000-0000-0000-0000-000000000002'.replace(/\-/gi, ''),
+    properties: ['read', 'write', 'notify', 'writeWithoutResponse'],
+    descriptors: [
+      new bleno.Descriptor({
+        uuid: '2901',
+        value: 'DENSe protocol',
+      }),
+    ],
+  });
+  this._tmpMessages = '';
+  this._updateValueCallback = null;
+}
+
+util.inherits(DENSeCharacteristic2, bleno.Characteristic);
+
+DENSeCharacteristic2.prototype.onWriteRequest = function (data, offset, withoutResponse, callback) {
+  console.log('inbound write req');
+
+  if (offset) {
+    callback(this.RESULT_ATTR_NOT_LONG);
+    return;
+  }
+
+  if (data.length === 0) {
+    callback(this.RESULT_INVALID_ATTRIBUTE_LENGTH);
+    return;
+  }
+
+  // if subscription active, notify every write request
+  if (this._updateValueCallback) {
+    this._updateValueCallback(data);
   }
 
   // store data, then write response
@@ -59,7 +125,7 @@ DENSeCharacteristic.prototype.onWriteRequest = function (data, offset, withoutRe
   callback(this.RESULT_SUCCESS);
 };
 
-DENSeCharacteristic.prototype.onReadRequest = function (offset, callback) {
+DENSeCharacteristic2.prototype.onReadRequest = function (offset, callback) {
   console.log('incoming read req');
 
   if (offset) {
@@ -68,43 +134,108 @@ DENSeCharacteristic.prototype.onReadRequest = function (offset, callback) {
   }
 
   // get data, then write response
-  if (!this._tmpMessages && !this._firstHalf && !this._secondHalf) {
+  if (!this._tmpMessages) {
     console.log('you have no messages stored');
     callback(this.RESULT_SUCCESS, Buffer.from('EOF', 'utf8'));
   } else {
-    if (this._firstHalf) {
-      console.log('sending first half');
-      callback(this.RESULT_SUCCESS, Buffer.from(this._firstHalf, 'utf8'));
-      if (this._secondHalf) {
-        console.log('sending second half');
-        callback(this.RESULT_SUCCESS, Buffer.from(this._secondHalf, 'utf8'));
-      }
-      return; // tmp
-    }
-
-    if (this._tmpMessages) {
-      console.log('sending stored messages');
-      callback(this.RESULT_SUCCESS, Buffer.from(this._tmpMessages, 'utf8'));
-    }
-    // const data = this._tmpMessages.toString();
-    // console.log('getting messages:', data);
-    // data.replace('undefined', '');
-    // callback(this.RESULT_SUCCESS, Buffer.from(data, 'utf8'));
+    const data = this._tmpMessages.toString();
+    console.log('getting messages:', data);
+    data.replace('undefined', '');
+    callback(this.RESULT_SUCCESS, Buffer.from(data, 'utf8'));
   }
 };
 
 // handle on subscribe
-DENSeCharacteristic.prototype.onSubscribe = function (maxValueSize, updateValueCallback) {
+DENSeCharacteristic2.prototype.onSubscribe = function (maxValueSize, updateValueCallback) {
   console.log('on subscribe');
 
   this._updateValueCallback = updateValueCallback;
 };
 
 // handle on unsubscribe
-DENSeCharacteristic.prototype.onUnsubscribe = function () {
+DENSeCharacteristic2.prototype.onUnsubscribe = function () {
   console.log('on unsubscribe');
 
   this._updateValueCallback = null;
 };
 
-module.exports = DENSeCharacteristic;
+function DENSeCharacteristic3() {
+  bleno.Characteristic.call(this, {
+    uuid: '20000000-0000-0000-0000-000000000002'.replace(/\-/gi, ''),
+    properties: ['read', 'write', 'notify', 'writeWithoutResponse'],
+    descriptors: [
+      new bleno.Descriptor({
+        uuid: '2901',
+        value: 'DENSe protocol',
+      }),
+    ],
+  });
+  this._tmpMessages = '';
+  this._updateValueCallback = null;
+}
+
+util.inherits(DENSeCharacteristic3, bleno.Characteristic);
+
+DENSeCharacteristic3.prototype.onWriteRequest = function (data, offset, withoutResponse, callback) {
+  console.log('inbound write req');
+
+  if (offset) {
+    callback(this.RESULT_ATTR_NOT_LONG);
+    return;
+  }
+
+  if (data.length === 0) {
+    callback(this.RESULT_INVALID_ATTRIBUTE_LENGTH);
+    return;
+  }
+
+  // if subscription active, notify every write request
+  if (this._updateValueCallback) {
+    this._updateValueCallback(data);
+  }
+
+  // store data, then write response
+  if (data.toString().includes('{') && data.toString().includes('header')) {
+    this._tmpMessages = data;
+  } else {
+    this._tmpMessages += data;
+  }
+  console.log('storing messages:', data.toString());
+  callback(this.RESULT_SUCCESS);
+};
+
+DENSeCharacteristic3.prototype.onReadRequest = function (offset, callback) {
+  console.log('incoming read req');
+
+  if (offset) {
+    callback(this.RESULT_ATTR_NOT_LONG, null);
+    return;
+  }
+
+  // get data, then write response
+  if (!this._tmpMessages) {
+    console.log('you have no messages stored');
+    callback(this.RESULT_SUCCESS, Buffer.from('EOF', 'utf8'));
+  } else {
+    const data = this._tmpMessages.toString();
+    console.log('getting messages:', data);
+    data.replace('undefined', '');
+    callback(this.RESULT_SUCCESS, Buffer.from(data, 'utf8'));
+  }
+};
+
+// handle on subscribe
+DENSeCharacteristic3.prototype.onSubscribe = function (maxValueSize, updateValueCallback) {
+  console.log('on subscribe');
+
+  this._updateValueCallback = updateValueCallback;
+};
+
+// handle on unsubscribe
+DENSeCharacteristic3.prototype.onUnsubscribe = function () {
+  console.log('on unsubscribe');
+
+  this._updateValueCallback = null;
+};
+
+module.exports = { DENSeCharacteristic1, DENSeCharacteristic2, DENSeCharacteristic3 };
--- service.js
+++ service.js
@@ -1,13 +1,21 @@
 var util = require('util');
 var bleno = require('bleno');
 
-const DENSeCharacteristic = require('./characteristic');
+const {
+  DENSeCharacteristic1,
+  DENSeCharacteristic2,
+  DENSeCharacteristic3,
+} = require('./characteristic');
 
 // create food bank service, then register food bank characteristic to it
 function DENSeService() {
   bleno.PrimaryService.call(this, {
     uuid: '10000000-0000-0000-0000-000000000001'.replace(/\-/gi, ''),
-    characteristics: [new DENSeCharacteristic()],
+    characteristics: [
+      new DENSeCharacteristic1(),
+      new DENSeCharacteristic2(),
+      new DENSeCharacteristic3(),
+    ],
   });
 }
 
